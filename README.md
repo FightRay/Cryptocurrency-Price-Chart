4acda4bb768f01856409479c553b641a0677d557
Ray <fightxray@gmail.com>
2022-01-07 09:53:56 +0100

    Final commit

--- README.md
+++ README.md
@@ -15,5 +15,36 @@ npm run serve
 npm run build
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Please request access from the this hosted cors anywhere proxy in order to test locally
+
+# Cors Anywhere for local testing (temporary)
+https://cors-anywhere.herokuapp.com/corsdemo
+
+## Components
+
+CoinTable component, requires prop "config" to be passed
+Graph component (currently contained in Home.vue, didn't have the time to component it),
+acts in conjuction with CoinTable emits.
+
+### API Extractions
+
+# Checks prices every 10 seconds
+https://cmd.btcdirect.eu/v2/price-ticker/BTC,ETH,LTC,ADA,XRP,BNB,LINK,DOGE,VET,ALGO,BAT,DASH,BCH,ATOM,ETC,EOS,XMR,XLM,TRX,ZEC
+
+to round difference:
+Math.round((diff * 100)) / 100;
+
+# Check prices for all coins
+https://app-backend.btcdirect.eu/api/prices/
+
+# Price graph
+Request URL: https://cmd.btcdirect.eu/v2/history/rates-graph/BTC/EUR
+Returns a 2-dimensional array with timestamp, price values
+
+# Delta price changes 24h
+https://cmd.btcdirect.eu/history/rates-delta/BTC,ETH,LTC,ADA,XRP,BNB,LINK,DOGE,VET,ALGO,BAT,DASH,BCH,ATOM,ETC,EOS,XMR,XLM,TRX,ZEC/EUR/24
+
+# Delta price changes 1y, called for every coin on eur/dollar toggle
+https://cmd.btcdirect.eu/v2/market-data/ZEC/EUR?datapoints[]=1y
+
+returns { roi: { 1y: value } }
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,132 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import Vue from "vue";
+	import VueApexCharts from 'vue-apexcharts'
+	Vue.use(VueApexCharts)
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	Vue.component('apexchart', VueApexCharts)
+
+	export default {
+		data() {
+			return {
+				proxy: true,
+				proxyUrl: "https://cors-anywhere.herokuapp.com/",
+				coinInfo: {
+					BTC: { name: "Bitcoin" },
+					LTC: { name: "Litecoin" },
+					ADA: { name: "Cardano" },
+					ALGO: { name: "Algorand" },
+					ATOM: { name: "Cosmos" },
+					BAT: { name: "Basic Attention Token" },
+					BCH: { name: "Bitcoin Cash" },
+					BNB: { name: "Binance Coin" },
+					DASH: { name: "Dash" },
+					DOGE: { name: "Dogecoin" },
+					DOT: { name: "Polkadot" },
+					EOS: { name: "EOS" },
+					ETC: { name: "Ethereum Classic" },
+					ETH: { name: "Ethereum" },
+					ICX: { name: "ICON" },
+					KNC: { name: "Kyber Network Crystal v2" },
+					LINK: { name: "Chainlink" },
+					LSK: { name: "Lisk" },
+					NANO: { name: "XNO" },
+					OMG: { name: "OMG Network" },
+					QTUM: { name: "Qtum" },
+					SNX: { name: "Synthetix" },
+					TRX: { name: "TRON" },
+					VET: { name: "VeChain" },
+					WAVES: { name: "Waves" },
+					XLM: { name: "Stellar" },
+					XMR: { name: "Monero" },
+					XRP: { name: "Ripple" },
+					XTZ: { name: "Tezos" },
+					ZEC: { name: "Zcash" },
+					UNI: { name: "Uniswap" },
+					SOL: { name: "Solana" },
+					AAVE: { name: "Aave" },
+					GRT: { name: "The Graph" },
+					MANA: { name: "Decentraland" },
+					STORJ: { name: "Storj" },
+					YFI: { name: "yearn.finance" },
+					MATIC: { name: "Polygon" },
+					AXS: { name: "Axie Infinity" },
+					ANKR: { name: "Ankr" },
+					SAND: { name: "The Sandbox" },
+					REN: { name: "Ren" }
+				},
+			};
+		},
+		created() {
+			Vue.prototype.$app = this;
+			Vue.prototype.$fetch = this.fetch;
+		},
+		mounted() {},
+		methods: {
+			fetch(url, method, data, actions, actionf, hasfile = false) {
+				if (this.proxy) {
+					url = this.proxyUrl + url;
+				}
+
+				let headers = new Headers();
+				headers.append("X-Requested-With", "XMLHttpRequest");
+
+				let requestData = {
+					method: method,
+					headers: headers,
+					redirect: "follow",
+					mode: "cors",
+				};
+
+				if (method == "POST") {
+					headers.append("Content-Type", "application/x-www-form-urlencoded");
+					let dataEncoded = hasfile ? new FormData() : new URLSearchParams();
+					for (let key in data) {
+						dataEncoded.append(key, data[key]);
+					}
+					requestData["body"] = dataEncoded;
+				}
+
+				fetch(url, requestData)
+					.then((response) => response.json())
+					.then((result) => actions(result))
+					.catch((error) => actionf(error));
+			},
+		},
+	};
 </script>
 
+<template>
+	<div id="app">
+		<router-view />
+	</div>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body {
+  margin: 0;
 }
+
+#app {
+  width: 100vw;
+  height: 100vh;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+}
+
+@font-face {
+  font-family: "GT-Walsheim-medium";
+  src: local("GT-Walsheim-medium"),
+   url("assets/fonts/GT-Walsheim-Medium.woff2") format("woff2");
+}
+
+@font-face {
+  font-family: "GT-Walsheim-regular";
+  src: local("GT-Walsheim-regular"),
+   url("assets/fonts/GT-Walsheim-Regular.woff2") format("woff2");
+}
+
 </style>
